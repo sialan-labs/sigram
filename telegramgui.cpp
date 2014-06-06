58e63be31ca752dc60a06b11112935d04ecde373
Bardia Daneshvar <bardia.daneshvar@gmail.com>
2014-06-06 18:44:01 +0430

    license

--- telegramgui.cpp
+++ telegramgui.cpp
@@ -1,3 +1,21 @@
+/*
+    Copyright (C) 2014 Sialan Labs
+    http://labs.sialan.org
+
+    Sigram is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Sigram is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #define NOTIFY_ACT_SHOW 0
 #define NOTIFY_ACT_MUTE 1
 #define NOTIFY_ACT_RMND 2
