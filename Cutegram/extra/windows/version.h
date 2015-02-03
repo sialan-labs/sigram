844a8d011f90132512fc6a22da0ceb4427e25224
unknown <bardia@Win8VM.(none)>
2015-02-02 22:44:19 -0800

    Fix windows build bug

--- Cutegram/extra/windows/version.h
+++ Cutegram/extra/windows/version.h
@@ -1,16 +1,16 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             1.0.0.0
-#define VER_FILEVERSION_STR         "1.0.0.0\0"
+#define VER_FILEVERSION             1,0,0
+#define VER_FILEVERSION_STR         "1.0.0\0"
 
 #define VER_PRODUCTVERSION          2,0,0
 #define VER_PRODUCTVERSION_STR      "2.0\0"
 
 #define VER_COMPANYNAME_STR         "Aseman Team"
-#define VER_FILEDESCRIPTION_STR     "Cutegram is a telegram client"
+#define VER_FILEDESCRIPTION_STR     "Cutegram is a better telegram client. It's Free and Open source."
 #define VER_INTERNALNAME_STR        "Cutegram"
-#define VER_LEGALCOPYRIGHT_STR      "Copyright © 2015 Aseman Co"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright (C) 2015 Aseman Co"
 #define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
 #define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
 #define VER_ORIGINALFILENAME_STR    "cutegram.exe"
