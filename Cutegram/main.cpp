4888cb6bbc2bb165db3d80960c2d094587f46aab
Bardia Daneshvar <bardia.daneshvar@gmail.com>
2015-05-29 18:58:53 +0430

    Add command line options to show help, version and verbose mode

--- Cutegram/main.cpp
+++ Cutegram/main.cpp
@@ -1,6 +1,6 @@
-#include "asemantools/asemanapplication.h"
-#include "asemantools/asemanquickview.h"
+#define ABOUT_TEXT "Cutegram is a free and opensource telegram clients, released under the GPLv3 license."
 
+#include "asemantools/asemanapplication.h"
 #include "cutegram.h"
 #include "telegramtestobject.h"
 #include "compabilitytools.h"
@@ -8,12 +8,12 @@
 #include <QMainWindow>
 #include <QPalette>
 #include <QNetworkProxy>
+#include <QCommandLineParser>
+
 #include <telegram.h>
 
 int main(int argc, char *argv[])
 {
-    qputenv("QT_LOGGING_RULES", "tg.*=false");
-
     AsemanApplication app(argc, argv);
     app.setApplicationName("Cutegram");
     app.setApplicationDisplayName("Cutegram");
@@ -23,14 +23,27 @@ int main(int argc, char *argv[])
     app.setWindowIcon(QIcon(":/qml/Cutegram/files/icon.png"));
     app.setQuitOnLastWindowClosed(false);
 
+    QCommandLineOption verboseOption(QStringList() << "V" << "verbose",
+            QCoreApplication::translate("main", "Verbose Mode."));
+    QCommandLineOption forceOption(QStringList() << "f" << "force",
+            QCoreApplication::translate("main", "Force to run multiple instance of Cutegram."));
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription(ABOUT_TEXT);
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addOption(forceOption);
+    parser.addOption(verboseOption);
+    parser.process(app);
+
+    if(!parser.isSet(verboseOption))
+        qputenv("QT_LOGGING_RULES", "tg.*=false");
+
     Telegram::setDefaultHostAddress("149.154.167.50");
     Telegram::setDefaultHostPort(443);
     Telegram::setDefaultHostDcId(2);
     Telegram::setAppId(13682);
     Telegram::setAppHash("de37bcf00f4688de900510f4f87384bb");
-
-//    TelegramTestObject testObj;
-//    return app.exec();
 
     if(app.readSetting("Proxy/enable",false).toBool())
     {
@@ -57,7 +70,7 @@ int main(int argc, char *argv[])
 #endif
 
 #ifdef DESKTOP_DEVICE
-    if( !app.arguments().contains("--force") && app.isRunning() )
+    if( !parser.isSet(forceOption) && app.isRunning() )
     {
         app.sendMessage("show");
         return 0;
